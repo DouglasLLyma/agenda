69945f5e5e91bd45fbc44d2a87f540abfb75ee2b
LLyma <douglasllyma@hotmail.com>
2022-07-20 17:46:01 -0300

    Criado tabelas de evento para trabalhar com a agenda

--- agenda/settings.py
+++ agenda/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -123,4 +124,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #
-# 
+#
